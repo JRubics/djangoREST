5cbdc438c561c54ccda74e31c4c027281f4bca5c
Jelena Dokic <jelena.dpk@gmail.com>
2019-03-15 16:20:06 +0100

    delete

--- restapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
-
-class Hotel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100)
-    star_num = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
