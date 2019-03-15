8aaa2c5d2103e37a3e21a014f9f5a1ed7a2db85a
Jelena Dokic <jelena.dpk@gmail.com>
2019-03-15 16:28:52 +0100

    frontend

--- frontend/index.js
+++ frontend/index.js
@@ -12,3 +12,18 @@ function showHotels(event) {
         },
     });
 }
+
+function showHotelNo5(event) {
+    $.ajax({
+        url : "http://localhost:8000/hotels/5/",
+        type:"GET",
+		contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success : function (data) {
+            alert(JSON.stringify(data))
+        },
+        error: function (message) {
+            alert(message.status)
+        },
+    });
+}
